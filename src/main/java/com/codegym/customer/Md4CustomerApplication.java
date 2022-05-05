8360b5323821d47605f7612c2550733697560437
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-05-06 00:22:06 +0700

    add

---
+++ src/main/java/com/codegym/customer/Md4CustomerApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4CustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4CustomerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codegym/customer/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.codegym.customer.service;
+
+import com.codegym.customer.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
